88d52ac0086414e62c670357429700498c611dc4
mac <khramtsov92alexander@gmail.com>
2024-01-22 15:47:40 +0300

    feat: Added the js files and populated them with content.

---
+++ js/hangmanBody_pics.js
@@ -0,0 +1,35 @@
+const arrayHangmanBody = [
+    {
+        className: 'hangman_img',
+        srcPics: './img/Hangman_game.svg'
+    },
+    {
+        className: 'hangman__head_pics',
+        srcPics: './img/head.svg'
+    },
+
+    {
+        className: 'hangman__body_pics',
+        srcPics: './img/body.svg'
+    },
+
+    {
+        className: 'left__hand_pics',
+        srcPics: './img/hand_left.svg'
+    },
+
+    {
+        className: 'right__hand_pics',
+        srcPics: './img/hand_right.svg'
+    },
+
+    {
+        className: 'left__leg_pics',
+        srcPics: './img/leg_left.svg'
+    },
+
+    {
+        className: 'right__leg_pics',
+        srcPics: './img/leg_right.svg'
+    },
+];
---
+++ js/word_list.js
@@ -0,0 +1,31 @@
+const wordList = [
+    {
+        word: 'apple',
+        hint: 'A fruit with two letters "p"'
+
+    },
+    {
+        word: 'lion',
+        hint: 'The king of beasts'
+    },
+    {
+        word: 'maps',
+        hint: "It's used to play poker!"
+    },
+    {
+        word: 'clock',
+        hint: 'There are wall-mounted and wrist-worn'
+    },
+    {
+        word: 'snow',
+        hint: 'Comes in winter instead of rain'
+    },
+    {
+        word: 'rainbow',
+        hint: 'Appears after the rain when the sun comes out'
+    },
+    {
+        word: 'spruce',
+        hint: 'Winter and summer in the same color'
+    }
+];
